8c3537c5a4563595e1c700dde24959458e042cd8
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2022-07-31 14:11:53 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/jarvis/android_jarvis_interface/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jarvis.android_jarvis_interface
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController emailControl, passwordControl;
+
+  @override
+  void initState() {
+    emailControl = TextEditingController();
+    passwordControl = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailControl.dispose();
+    passwordControl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register")),
+      body: Column(
+        children: [
+          TextField(
+            controller: emailControl,
+            decoration: const InputDecoration(
+              hintText: "Input email here",
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10)
+                ),
+                borderSide: BorderSide(
+                  color: Colors.green
+                )
+              )
+            ),
+          ),
+          TextField(
+            controller: passwordControl
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = emailControl.text;
+              final password = passwordControl.text;
+            },
+            child: const Text("Register")),
+        ],
+      ),
+    );
+  }
+}
+
